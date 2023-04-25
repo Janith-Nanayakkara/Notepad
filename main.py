1b4b71ec9016123104c715fb52b43e42af692c0b
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-04-25 18:39:53 +0530

    open save clear exit button added

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ canvas = tk.Tk()
 canvas.geometry("400x600")
 canvas.title("Notepad")
 canvas.config(bg = "white")
+top= tk.Frame(canvas)
+top.pack(padx=10,pady=5,anchor="nw")
 
 b1 = tk.Button(canvas, text ="open", bg ="white")
-b1.pack(side=LEFT)
+b1.pack(in_=top ,side=LEFT)
 tk.Button
 b2 = tk.Button(canvas, text ="save", bg ="white")
-b2.pack(side=LEFT)
+b2.pack(in_=top,side=LEFT)
+
+b3 = tk.Button(canvas, text ="clear", bg ="white")
+b3.pack(in_=top,side=LEFT)
+
+b4 = tk.Button(canvas, text ="exit", bg ="white")
+b4.pack(in_=top,side=LEFT)
 canvas.mainloop()
 
 
